00f50dc31eb9f23a197688fa8e37985b680cddd4
Sonu Prajapati <sonuprajapati83497@gmail.com>
2025-11-15 15:05:38 +0530

    basic list accessing using while loop

---
+++ list.py
@@ -0,0 +1,9 @@
+#Accessing using while loop
+a = [10,20,11.3,15.5,"CyberSecurity"]
+print("Accessing List using while loop")
+n = len(a)
+print(n)
+i=0
+while i<n:
+    print(i,"=",a[i])
+    i +=1 
