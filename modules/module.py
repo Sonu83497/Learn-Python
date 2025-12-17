d480270fe4420277e60af67c4763c6cc19761cc5
Sonu Prajapati <sonuprajapati83497@gmail.com>
2025-12-17 09:31:54 +0530

    update

---
+++ modules/module.py
@@ -0,0 +1,7 @@
+import myModule
+myModule.say_hello("sonu")
+
+myModule.say_bye("sonu")
+
+from myModule import per # import /  use specific part of code
+print(per)
---
+++ modules/myModule.py
@@ -0,0 +1,12 @@
+def say_hello(name):
+    return print(f"Hello, {name}!")
+
+def say_bye(name):
+    return print(f"Goodbye, {name}!")
+
+
+per  = {
+    "name" : "sonu",
+    "age" : 20,
+    "gender" : "male"
+}
